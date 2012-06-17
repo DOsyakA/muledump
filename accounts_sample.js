5ef1aff3b8e2e4762ff7932da4a073cf0bfd8b4a
DOsyakA <maksimich95@mail.ru>
2012-06-17 23:30:29 +0600

    Update master

--- accounts_sample.js
+++ accounts_sample.js
@@ -7,18 +7,18 @@ accounts = {
 // escape them with backslashes (\) like this: 'pass\'word'
 
 
-'email': 'pass',
-'email2': 'pass2',
+'maksimich95@mail.ru': 'e543431245',
+'WKOLOLOLO@mail.ru': '123456789a',
 
 
 }// don't delete this line!
 
 
 // change to 1 to switch to testing
-testing = 0
+testing = 1
 
 // change to 1 to enable price display in tooltips
 prices = 0
 
 // change to 1 to enable one-click login (run lib/mulelogin.au3 first)
-mulelogin = 0
+mulelogin = 1
